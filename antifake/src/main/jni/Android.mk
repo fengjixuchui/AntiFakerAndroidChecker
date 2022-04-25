1c1b78277e509e960ead8a61bba71649de3836f9
李尚 <hzlishang@corp.netease.com>
2022-04-25 15:13:21 +0800

    配置失败，还是要单独产出 不同 的src

--- antifake/src/main/jni/Android.mk
+++ antifake/src/main/jni/Android.mk
@@ -9,20 +9,6 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_LDLIBS:=-L$(SYSROOT)/usr/lib -llog
-LOCAL_MODULE    := emulator_check
-LOCAL_MULTILIB  := '64'
-LOCAL_SRC_FILES := emulator/com.snail.antifake_jni_EmulatorDetectUtil64.c
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_LDLIBS:=-L$(SYSROOT)/usr/lib -llog
-LOCAL_MODULE    := emulator_check
-LOCAL_MULTILIB  := 'x86'
-LOCAL_SRC_FILES := emulator/com.snail.antifake_jni_EmulatorDetectUtil86.c
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_LDLIBS:=-L$(SYSROOT)/usr/lib -llog
 LOCAL_MODULE    := property_get
 LOCAL_SRC_FILES := property/com.snail.antifake_jni_PropertiesGet.c
 include $(BUILD_SHARED_LIBRARY)
